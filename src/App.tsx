c26b3df6aea7a91484a8bc22a3ccefd54b3a6bd1
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-06-28 22:10:48 +0530

    frontend init

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center h-screen ">
+      <img src="/logo-dark.png" className="rounded-2xl h-80" alt="" />
+    </div>
+  )
+}
