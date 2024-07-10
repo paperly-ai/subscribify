1644b20a4987e151c6b87b0c4e7f177c63a5c129
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-07-11 00:34:24 +0530

    uploading documents and handling loading state

--- src/api/fileUpload.ts
+++ src/api/fileUpload.ts
@@ -1,17 +1,28 @@
-export const handleFileUpload = (file: File) => {
+import axios from "axios";
+import { SERVER_URL } from "./config";
+import toast from "react-hot-toast";
+
+export const documentUpload = async (file: File) => {
   console.log('Selected file:', file);
+
   const formData = new FormData();
   formData.append('pdf', file);
 
-  fetch('http://localhost:8080/api/upload', {
-    method: 'POST',
-    body: formData,
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Upload successful:', data);
-    })
-    .catch(error => {
-      console.error('Upload error:', error);
+  const token = localStorage.getItem('accessToken');
+
+  try {
+    const response = await axios.post(`${SERVER_URL}/api/pdf/upload`, formData, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
     });
+
+    toast.success("Document uploaded successfully");
+    console.log('Upload response:', response.data);
+    return true;
+  } catch (error) {
+    toast.error("Error uploading document");
+    console.error('Upload error:', error);
+    return false; // Optionally handle the error or rethrow it
+  }
 };
