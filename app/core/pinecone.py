7eaddda2965c866bc85b873f528d01e54032cef2
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-07-13 19:47:06 +0530

    pinecone config updated

--- app/core/pinecone.py
+++ app/core/pinecone.py
@@ -1,11 +1,34 @@
 import pinecone
 from app.core.config import settings
 
+import os
+from pinecone import Pinecone, ServerlessSpec
+
+
+
+
+    
+
+
+
 def init_pinecone():
-    pinecone.init(api_key=settings.PINECONE_API_KEY, environment=settings.PINECONE_ENVIRONMENT)
     index_name = "pdf-chat-index"
-    if index_name not in pinecone.list_indexes():
-        pinecone.create_index(index_name, dimension=512)
-    return pinecone.Index(index_name)
+
+    pc = Pinecone(
+    api_key=settings.PINECONE_API_KEY
+    )
+
+    if 'pdf-chat-index' not in pc.list_indexes().names():
+        pc.create_index(
+            name='pdf-chat-index', 
+            dimension=1536, 
+            metric='euclidean',
+            spec=ServerlessSpec(
+                cloud='aws',
+                region='us-east-1'
+            )
+        )
+        
+    return pc.Index(index_name)
 
 pinecone_index = init_pinecone()
