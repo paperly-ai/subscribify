13791fa9ba8e79f3a7deb3c33478c8b443e1951e
lokeshdarla <lokeshnagasaidarla@gmail.com>
2024-07-15 23:43:14 +0530

    updated config

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,12 @@ class Settings(BaseSettings):
     PINECONE_ENVIRONMENT: str
     GOOGLE_API_KEY:str
     GOOGLE_EMBEDDING_MODEL:str
+    PINECONE_INDEX_NAME:str
+    VECTOR_DIMENSIONS:int
+    PINECONE_METRIC:str
+    PINECONE_CLOUD:str
+    PINCEONCE_REGION:str
+
 
     class Config:
         env_file = ".env"
